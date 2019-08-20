a36dacec3645e5752c646d2a7111c808d814c1e9
eulegang <eulegang@gmail.com>
2019-08-20 11:42:36 -0500

    Dryed up feature detection with macro

--- src/features.rs
+++ src/features.rs
@@ -1,10 +1,16 @@
 
+macro_rules! feature {
+    ($feat: expr) => {
+        (cfg!(feature = $feat), $feat.to_string())
+    }
+}
+
 /// Gives a record of which features romulus was installed with
 pub fn features() -> Vec<(bool, String)> {
     vec![
-        (cfg!(feature = "color"), "color".to_string()),
-        (cfg!(feature = "envvar"), "envvar".to_string()),
-        (cfg!(feature = "stdin-tty"), "stdin-tty".to_string()),
+        feature!("color"),
+        feature!("envvar"),
+        feature!("stdin-tty")
     ]
 }
 
