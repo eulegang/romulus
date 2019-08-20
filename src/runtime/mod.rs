2da5abc26fcde78a8213a2026a9b91339ceb9fc4
eulegang <eulegang@gmail.com>
2019-08-20 11:03:35 -0500

    Removing value type
    This was moved to just strings a while ago

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -3,9 +3,7 @@
 mod environment;
 mod range_scope_tracker;
 mod scope;
-mod val;
 
 pub use environment::{Environment, Event};
 pub(crate) use range_scope_tracker::RangeScopeTracker;
 pub use scope::Scope;
-pub use val::Value;
