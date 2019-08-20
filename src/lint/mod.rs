3ad9a0f715a71d4f6936910fb887b268d65ed741
eulegang <eulegang@gmail.com>
2019-08-20 13:19:33 -0500

    Documenting lint module

--- src/lint/mod.rs
+++ src/lint/mod.rs
@@ -1,16 +1,22 @@
+//! A module which lints a romulus program
 
 use crate::ast::Seq;
 use std::fmt;
 
 mod vars;
 
+/// A wrapper type around an error message
 #[derive(Clone, PartialEq)]
 pub struct LintMessage(String);
 
-pub trait Lint {
+pub(crate) trait Lint {
     fn lint(&self, node: &Seq) -> Vec<LintMessage>;
 }
 
+/// Lints a romulus program with standard linters
+///
+/// Current linters 
+/// 1. undefined variables
 pub fn lint(node: &Seq) -> Vec<LintMessage> {
     let mut results = Vec::new();
 
